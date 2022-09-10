1a2e906259c2a5c4c185ad704d8f03a28efa60ee
renan-peixoto <renan2.peixoto@gmail.com>
2022-09-10 18:00:52 -0300

    tentativas 3

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 let numeroAleatorio = parseInt(Math.random() * 11);
+let tentativas = 0;
 
 function Chutar() {
     const chute = parseInt(document.getElementById("valor").value);
     const elementoResultado = document.getElementById("resultado");
+    const elementoTentativas = document.getElementById("tentativas");
 
     if (chute == numeroAleatorio) {
-        elementoResultado.innerHTML = "Você acerto!"
+        elementoResultado.innerHTML = "você acertou!"
+        
+        console.log(tentativas)
     } else if (chute > 10 || chute < 0) {
-        elementoResultado.innerHTML = "Você deve escolher um número entre 0 e 10."
+        elementoResultado.innerHTML = `Você deve escolher um número entre 0 e 10. \n tentativa: ${tentativas}`
+        tentativas++
+        
     } else {
-        elementoResultado.innerHTML = "Você errou!"
+        elementoResultado.innerHTML = `você errou! \n tentativa: ${tentativas}`
+        tentativas++
+        
     }
+    if (tentativas > 3) {
+        elementoTentativas.innerHTML = `Você gastou suas tentativas, a resposta certa é: ${numeroAleatorio}`
+    }
+
 }
+
+
